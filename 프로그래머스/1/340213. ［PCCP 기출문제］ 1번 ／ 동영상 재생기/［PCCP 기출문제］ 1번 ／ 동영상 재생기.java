527c514648bb96286ee955ca442e617bd3956a1a
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-04-16 13:44:57 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 8.58 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,44 @@
+// for문을 돌면서 command를 수행
+// command를 수행하기 전에 먼저 '오프닝 건너뛰기' 판단
+
+class Solution {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        // 시간 초 단위로 변환
+        int videoLen = parsingTime(video_len);
+        int curTime = parsingTime(pos);
+        int opStart = parsingTime(op_start);
+        int opEnd = parsingTime(op_end);
+        
+        for (int i = 0; i < commands.length; i++) {
+            curTime = opJump(curTime, opStart, opEnd);
+            if (commands[i].equals("next"))
+                curTime = next(curTime, videoLen);
+            else
+                curTime = prev(curTime);
+        }
+        curTime = opJump(curTime, opStart, opEnd);
+        
+        // 이거 분, 초 두 자리로 나오게 해야됨.
+        return String.format("%02d", curTime / 60) + ":" + String.format("%02d", curTime % 60);
+    }
+    
+    private int parsingTime(String time) {
+        String[] timeArr = time.split(":");
+        return Integer.parseInt(timeArr[0]) * 60 + Integer.parseInt(timeArr[1]);
+    }
+    
+    private int next(int curTime, int endTime) {
+        curTime += 10;
+        return curTime > endTime ? endTime : curTime;
+    }
+    
+    private int prev(int curTime) {
+        curTime -= 10;
+        return curTime > 0 ? curTime : 0;
+    }
+    
+    private int opJump(int curTime, int opStart, int opEnd) {
+        return curTime >= opStart && curTime <= opEnd ?
+            opEnd : curTime;
+    }
+}

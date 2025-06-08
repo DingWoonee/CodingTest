02e3e330ea6b9725e1749130f9551a048d670f05
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-08 23:59:15 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,29 +1,33 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int w, int num) {
-        // 완전한 줄 수
-        int completeRowNum = n / w;
-        // 타겟 줄 위치
-        int targetRowNum = (num - 1) / w + 1;
-        // 맨 윗 줄 블럭 수
-        int topNum = n % w;
-        // 타겟의 열
-        int targetColumn 
-            = ((num - 1) / w) % 2 == 0 ? (num % w == 0 ? w : num % w) : w + 1 - (num % w == 0 ? w : num % w);
-        // 결과
-        int answer = completeRowNum - targetRowNum;
-        if (answer <= 0)
-            return 1;
-        else if (topNum == 0)
-            return answer + 1;
-        // 맨 윗 줄 거를 꺼내야 하는지
-        if (completeRowNum % 2 == 0) { // 맨 윗 줄이 왼쪽에서 시작
-            if (topNum >= targetColumn)
-                answer++;
-        } else { // 맨 윗 줄이 오른쪽에서 시작
-            if (targetColumn + topNum > w)
-                answer++;
+        // 완전한 줄 수?
+        int prefectRowNum = n / w;
+        // 아래에서 몇 번째 줄?
+        int floor = (num - 1) / w + 1;
+        // 현재 몇 번재 열?
+        int col = num % w == 0 ? w : num % w;
+        if (floor % 2 == 0) {
+            col = w + 1 - col;
+        }
+        // 해당 열의 맨 윗 줄에 택배가 있는지?
+        int isTopOn = 0;
+        // 맨 윗 줄 택배 개수?
+        int topCount = n % w;
+        // 맨 위에 택배가 있다면
+        if (topCount != 0) {
+            if (prefectRowNum % 2 == 0) { // 방향 - 우
+                if (topCount >= col) {
+                    isTopOn++;
+                }
+            } else { // 방향 - 좌
+                if (col > w - topCount) {
+                    isTopOn++;
+                }
+            }
         }
         
-        return answer + 1;
+        return prefectRowNum - floor + 1 + isTopOn;
     }
 }

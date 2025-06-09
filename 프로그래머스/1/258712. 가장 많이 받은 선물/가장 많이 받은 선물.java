f91a4edca98e0afd282b9eba18f13b76bc435e09
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-09 20:27:37 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,44 +1,42 @@
 import java.util.*;
+import java.util.stream.*;
 
+/**
+* 알아야 하는 것
+* 1. 각 사람의 선물 지수
+* 2. 서로 선물을 준 횟수
+*/
 class Solution {
+    
     public int solution(String[] friends, String[] gifts) {
-        // 선물을 준 사람과 받은 사람
-        int[][] relation = new int[friends.length][friends.length];
-        // 선물 지수
-        int[] pScore = new int[friends.length];
-        // 인덱스
-        Map<String, Integer> index = new HashMap<>();
-        for (int i = 0; i < friends.length; i++) {
-            index.put(friends[i], i);
-        }
-        // 선물 내역 분석
+        int[] giftScore = new int[friends.length];
+        int[][] table = new int[friends.length][friends.length];
+        Map<String, Integer> toIndex = new HashMap<>();
+        IntStream.range(0, friends.length).forEach(n -> toIndex.put(friends[n], n));
+        
         for (String gift : gifts) {
-            String[] split = gift.split(" ");
-            String giver = split[0];
-            String receiver = split[1];
+            String giver = gift.split(" ")[0];
+            String receiver = gift.split(" ")[1];
+            int giverIndex = toIndex.get(giver);
+            int receiverIndex = toIndex.get(receiver);
             
-            int giverIdx = index.get(giver);
-            int receiverIdx = index.get(receiver);
-            relation[giverIdx][receiverIdx] += 1;
-            pScore[giverIdx] += 1;
-            pScore[receiverIdx] -= 1;
+            table[giverIndex][receiverIndex]++;
+            giftScore[giverIndex]++;
+            giftScore[receiverIndex]--;
         }
-        // 답 구하기
+        
         int answer = 0;
         for (int i = 0; i < friends.length; i++) {
             int count = 0;
             for (int j = 0; j < friends.length; j++) {
-                if (relation[i][j] > relation[j][i]) {
-                    count++;
-                    continue;
-                }
-                if (relation[i][j] == relation[j][i]
-                   && pScore[i] > pScore[j]) {
+                if (i == j) continue;
+                
+                if (table[i][j] > table[j][i]
+                   || (table[i][j] == table[j][i] && giftScore[i] > giftScore[j])) {
                     count++;
                 }
             }
-            if (count > answer)
-                answer = count;
+            answer = Math.max(count, answer);
         }
         return answer;
     }

2f09b23a958cd41b42b1c68231905cb84e3f825c
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-09 19:24:01 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.02 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,21 +1,20 @@
+import java.util.*;
+
 class Solution {
+    
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
         
         while (!isPossible(wallet, bill)) {
-            fold(bill);
+            if (bill[0] > bill[1]) {
+                bill[0] /= 2;
+            } else {
+                bill[1] /= 2;
+            }
             answer++;
         }
         
         return answer;
-    }
-    
-    private void fold(int[] bill) {
-        if (bill[0] > bill[1]) {
-            bill[0] = bill[0] / 2;
-        } else {
-            bill[1] = bill[1] / 2;
-        }
     }
     
     private boolean isPossible(int[] wallet, int[] bill) {

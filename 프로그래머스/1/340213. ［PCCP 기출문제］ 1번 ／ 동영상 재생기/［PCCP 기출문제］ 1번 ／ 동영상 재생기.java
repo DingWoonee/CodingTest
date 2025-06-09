7b799caefdf7902571b429f4d0bd111aff6a0713
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-09 18:57:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 8.36 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,44 +1,58 @@
-// for문을 돌면서 command를 수행
-// command를 수행하기 전에 먼저 '오프닝 건너뛰기' 판단
+import java.util.*;
 
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        // 시간 초 단위로 변환
-        int videoLen = parsingTime(video_len);
-        int curTime = parsingTime(pos);
-        int opStart = parsingTime(op_start);
-        int opEnd = parsingTime(op_end);
-        // 명령 수행
-        for (int i = 0; i < commands.length; i++) {
-            curTime = opJump(curTime, opStart, opEnd);
-            if (commands[i].equals("next"))
-                curTime = next(curTime, videoLen);
-            else
-                curTime = prev(curTime);
-        }
-        curTime = opJump(curTime, opStart, opEnd);
+        int videoLenInt = parse(video_len);
+        int posInt = parse(pos);
+        int opStartInt = parse(op_start);
+        int opEndInt = parse(op_end);
         
-        // 이거 분, 초 두 자리로 나오게 해야됨.
-        return String.format("%02d", curTime / 60) + ":" + String.format("%02d", curTime % 60);
+        for (String command : commands) {
+            // 건너뛰기
+            if (posInt >= opStartInt && posInt <= opEndInt) {
+                posInt = opEndInt;
+            }
+            if ("prev".equals(command)) { // prev
+                posInt = prev(posInt);
+            } else { // next
+                posInt = next(posInt, videoLenInt);
+            }
+        }
+        // 건너뛰기
+        if (posInt >= opStartInt && posInt <= opEndInt) {
+            posInt = opEndInt;
+        }
+        
+        return String.format("%02d", posInt / 100) + ":" + String.format("%02d", posInt % 100);
     }
     
-    private int parsingTime(String time) {
-        String[] timeArr = time.split(":");
-        return Integer.parseInt(timeArr[0]) * 60 + Integer.parseInt(timeArr[1]);
+    private int parse(String time) {
+        return Integer.parseInt(time.split(":")[0]) * 100 + Integer.parseInt(time.split(":")[1]);
     }
     
-    private int next(int curTime, int endTime) {
-        curTime += 10;
-        return curTime > endTime ? endTime : curTime;
+    private int prev(int pos) {
+        int minute = pos / 100;
+        int second = pos % 100;
+        if (minute == 0 && second <= 10) {
+            return 0;
+        }
+        if (second < 10) {
+            return (minute - 1) * 100 + (second + 50);
+        }
+        return pos - 10;
     }
     
-    private int prev(int curTime) {
-        curTime -= 10;
-        return curTime > 0 ? curTime : 0;
+    private int next(int pos, int videoLen) {
+        int minute = pos / 100;
+        int second = pos % 100;
+        if (second >= 50) {
+            minute++;
+            second -= 50;
+        } else {
+            second += 10;
+        }
+        int result = minute * 100 + second;
+        return Math.min(videoLen, result);
     }
     
-    private int opJump(int curTime, int opStart, int opEnd) {
-        return curTime >= opStart && curTime <= opEnd ?
-            opEnd : curTime;
-    }
 }

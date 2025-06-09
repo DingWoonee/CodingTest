7f209f5da8206ded964a988e50c99391333cbb0c
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-09 20:05:47 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.26 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,48 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int maxSize = 0;
+        int max = 0;
+        int[][] parkInt = new int[park.length][park[0].length];
         
         for (int i = 0; i < park.length; i++) {
             for (int j = 0; j < park[0].length; j++) {
-                maxSize = Math.max(maxSize, getMaxSize(park, i, j));
+                if ("-1".equals(park[i][j])) {
+                    int up = (i == 0 ? 0 : parkInt[i - 1][j]);
+                    int left = (j == 0 ? 0 : parkInt[i][j - 1]);
+                    int leftUp = (i == 0 || j == 0 ? 0 : parkInt[i - 1][j - 1]);
+                    parkInt[i][j] = Math.min(Math.min(up, left), leftUp) + 1;
+                    max = Math.max(max, parkInt[i][j]);
+                }
             }
         }
         
         int answer = -1;
-        for (int mat : mats) {
-            if (mat <= maxSize) {
-                answer = Math.max(answer, mat);
+        for (int m : mats) {
+            if (m <= max && m > answer) {
+                answer = m;
             }
         }
-        
         return answer;
-    }
-    
-    // 좌표 기준으로 오른쪽 아래 영역에서 가질 수 있는 가장 큰 영역 반환
-    private int getMaxSize(String[][] park, int i, int j) {
-        int answer = 0;
-        int parkWidth = park[0].length;
-        int parkHeight = park.length;
-        while (true) {
-            // 세로
-            for (int k = 0; k < answer + 1; k++) {
-                if (i + k >= parkHeight || j + answer >= parkWidth) {
-                    return answer;
-                }
-                if (!park[i + k][j + answer].equals("-1")) {
-                    return answer;
-                }
-            }
-            // 가로
-            for (int k = 0; k < answer + 1; k++) {
-                if (j + k >= parkWidth || i + answer >= parkHeight) {
-                    return answer;
-                }
-                if (!park[i + answer][j + k].equals("-1")) {
-                    return answer;
-                }
-            }
-            answer++;
-        }
     }
 }

0773136819bac913ef70636be12ba1b55e597037
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-11 22:54:32 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,24 +2,28 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        Map<Integer, String> ranks = new HashMap<>();
+        // Map<Integer, String> ranks = new HashMap<>();
         Map<String, Integer> names = new HashMap<>();
         
         for (int i = 0; i < players.length; i++) {
-            ranks.put(i + 1, players[i]);
+            // ranks.put(i + 1, players[i]);
             names.put(players[i], i + 1);
         }
         
         for (String name : callings) {
             int rank = names.get(name);
-            String subName = ranks.get(rank - 1);
-            ranks.put(rank - 1, name);
-            ranks.put(rank, subName);
+            // String subName = ranks.get(rank - 1);
+            String subName = players[rank - 2];
+            // ranks.put(rank - 1, name);
+            // ranks.put(rank, subName);
+            players[rank - 2] = name;
+            players[rank - 1] = subName;
             names.put(name, rank - 1);
             names.put(subName, rank);
         }
         
-        return ranks.values().stream()
-            .toArray(String[]::new);
+        // return ranks.values().stream()
+        //     .toArray(String[]::new);
+        return players;
     }
 }

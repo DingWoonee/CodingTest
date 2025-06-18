c3b4c94001a254f50836a98ed8476de0a0f0541f
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-18 16:43:43 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -5,19 +5,18 @@ class Solution {
         int answer = 0;
         
         int servers = 0;
-        Deque<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < k; i++) {
-            queue.offer(0);
-        }
+        int[] record = new int[24];
         
-        for (int player : players) {
+        for (int i = 0; i < players.length; i++) {
             // 서버 비활성화
-            servers -= queue.poll();
+            if (i >= k) {
+                servers -= record[i - k];
+            }
             // 0 ~ m-1 -> 0대 / m ~ 2m-1 -> 1대 / 2m ~ 3m-1 -> 2대
-            int addCnt = Math.max(player / m - servers, 0);
+            int addCnt = Math.max(players[i] / m - servers, 0);
             answer += addCnt;
             servers += addCnt;
-            queue.offer(addCnt);
+            record[i] = addCnt;
         }
         
         return answer;

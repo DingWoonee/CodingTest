b9bf2f88a9856ec8a5b13d9cc88dcca7cc92b4da
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-18 17:34:05 +0900

    [level 2] Title: 비밀 코드 해독, Time: 7.40 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,52 +2,41 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    /**
-    * 전체 경우의 수 -> nC5 = O(n^5) -> 전체 비교는 패스..
-    * DFS? -> 5 ^ q.length
-    */
-    Set<List<Integer>> result = new HashSet<>();
+    
+    private int answer = 0;
     
     public int solution(int n, int[][] q, int[] ans) {
-        Set<Integer> set = new TreeSet<>(); // 순서 유지를 위해 TreeSet 사용
+        int[] visited = new int[n + 1];
         
-        backtrack(n, q, ans, set, 1);
+        backtrack(n, q, ans, 0, visited, 1);
         
-        return result.size();
+        return answer;
     }
     
-    private void backtrack(int n, int[][] inputs, int[] ans, Set<Integer> set, int idx) {
-        if (set.size() == 5) {
+    private void backtrack(int n, int[][] inputs, int[] ans, int count, int[] visited, int idx) {
+        if (count == 5) {
             // 정답 체크
-            if (isRight(inputs, ans, set)) {
-                result.add(
-                    new ArrayList<>(set)
-                );
-            }
-            // 더 볼 필요 없어서 중간에 끊기
-            return;
-        }
-        // 백트래킹
-        for (int i = idx; i <= n; i++) {
-            set.add(i);
-            backtrack(n, inputs, ans, set, i + 1);
-            set.remove(i);
-        }
-    }
-    
-    private boolean isRight(int[][] inputs, int[] ans, Set<Integer> set) {
-        for (int i = 0; i < inputs.length; i++) {
-            int[] input = inputs[i];
-            int count = 0;
-            for (int num  : input) {
-                if (set.contains(num)) {
-                    count++;
+            for (int i = 0; i < inputs.length; i++) {
+                int[] input = inputs[i];
+                int sum = 0;
+                for (int num : input) {
+                    if (visited[num] == 1) {
+                        sum++;
+                    }
+                }
+                if (ans[i] != sum) {
+                    return;
                 }
             }
-            if (count != ans[i]) {
-                return false;
-            }
+            // 정답
+            answer++;
+            return;
         }
-        return true;
+        // 재귀호출
+        for (int i = idx; i <= n; i++) {
+            visited[i] = 1;
+            backtrack(n, inputs, ans, count + 1, visited, i + 1);
+            visited[i] = 0;
+        }
     }
 }

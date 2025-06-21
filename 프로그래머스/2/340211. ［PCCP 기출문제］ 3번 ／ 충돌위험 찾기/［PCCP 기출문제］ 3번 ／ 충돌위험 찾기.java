a1eace2f08372291d0441ad91114be51c61d03c4
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-06-21 18:02:27 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 213.63 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,67 +1,72 @@
-import java.util.*;
-
 class Solution {
     // points 조회할 때는 -1하기
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
-        // List: time, r, c
-        Map<String, Integer> events = new HashMap<>();
+        int robotNum = routes.length;
+        // 현재 위치
+        int[][] curPos = new int[routes.length][2];
+        // 현재 route 인덱스
+        int[] routeIdxs = new int[routes.length];
+        // 충돌 상태 체크를 위한 현위치 배열
+        int[][] map = new int[101][101];
         
-        for (int i = 0; i < routes.length; i++) {
-            int time = 0;
-            int[] route = routes[i];
-            for (int j = 0; j < route.length - 1; j++) {
-                int[] start = points[route[j] - 1];
-                int[] end = points[route[j + 1] - 1];
-                if (end[0] > start[0]) {
-                    for (int r = start[0]; r < end[0]; r++) {
-                        String event = time + " " +  r + " " + start[1];
-                        events.put(event, events.getOrDefault(event, 0) + 1);
-                        if (events.get(event) == 2) { // 중복 제거를 위해 딱 한번만 기록
-                            answer++;
-                        }
-                        time++;
-                    }
-                } else {
-                    for (int r = start[0]; r > end[0]; r--) {
-                        String event = time + " " +  r + " " + start[1];
-                        events.put(event, events.getOrDefault(event, 0) + 1);
-                        if (events.get(event) == 2) { // 중복 제거를 위해 딱 한번만 기록
-                            answer++;
-                        }
-                        time++;
-                    }
+        while (true) {
+            for (int i = 0; i < robotNum; i++) {
+                if (routeIdxs[i] >= routes[i].length) {
+                    continue;
                 }
-                if (end[1] > start[1]) {
-                    for (int c = start[1]; c < end[1]; c++) {
-                        String event = time + " " +  end[0] + " " + c;
-                        events.put(event, events.getOrDefault(event, 0) + 1);
-                        if (events.get(event) == 2) { // 중복 제거를 위해 딱 한번만 기록
-                            answer++;
-                        }
-                        time++;
-                    }
-                } else {
-                    for (int c = start[1]; c > end[1]; c--) {
-                        String event = time + " " +  end[0] + " " + c;
-                        events.put(event, events.getOrDefault(event, 0) + 1);
-                        if (events.get(event) == 2) { // 중복 제거를 위해 딱 한번만 기록
-                            answer++;
-                        }
-                        time++;
-                    }
+                // 최초
+                if (routeIdxs[i] == 0) {
+                    int[] point = points[routes[i][0] - 1];
+                    curPos[i][0] = point[0];
+                    curPos[i][1] = point[1];
+                    routeIdxs[i]++;
+                    map[curPos[i][0]][curPos[i][1]]++;
+                    continue;
                 }
-                
+                // 목적지 도달
+                int[] desPos = points[routes[i][routeIdxs[i]] - 1];
+                if (curPos[i][0] == desPos[0] && curPos[i][1] == desPos[1]) {
+                    routeIdxs[i]++;
+                    if (routeIdxs[i] >= routes[i].length) {
+                        continue;
+                    }
+                    desPos = points[routes[i][routeIdxs[i]] - 1];
+                }
+                // 이동
+                if (desPos[0] > curPos[i][0]) {
+                    curPos[i][0]++;
+                } else if (curPos[i][0] > desPos[0]) {
+                    curPos[i][0]--;
+                } else if (desPos[1] > curPos[i][1]) {
+                    curPos[i][1]++;
+                } else {
+                    curPos[i][1]--;
+                }
+                map[curPos[i][0]][curPos[i][1]]++;
             }
-            // 루트의 마지막 위치
-            int[] last = points[route[route.length - 1] - 1];
-            String event = time + " " +  last[0] + " " + last[1];
-            events.put(event, events.getOrDefault(event, 0) + 1);
-            if (events.get(event) == 2) { // 중복 제거를 위해 딱 한번만 기록
-                answer++;
+            // 충돌 체크
+            int count = 0;
+            for (int i = 1; i < map.length; i++) {
+                for (int j = 1; j < map[0].length; j++) {
+                    if (map[i][j] > 1) {
+                        count++;
+                    }
+                    map[i][j] = 0;
+                }
+            }
+            answer += count;
+            // 완료 체크
+            int checkCount = 0;
+            for (int i = 0; i < robotNum; i++) {
+                if (routeIdxs[i] >= routes[i].length) {
+                    checkCount++;
+                }
+            }
+            if (checkCount == robotNum) {
+                break;
             }
         }
-        
         return answer;
     }
 }

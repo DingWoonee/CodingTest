4da75518424fc18245d383789615cd673fdf55d8
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-07-29 14:59:05 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.74 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,11 +2,16 @@ import java.util.*;
 /**
 100만개 정렬 -> 최악의 경우 O(n^2) -> 100만 * 100만...
 근데 평균적으로 O(nlogn) -> 100만 * 20.. -> 할만한데?
+
+근데 풀고나서 봤는데 원소 길이가 500이었네;;
 */
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        Arrays.sort(data, Comparator.comparing((int[] d) -> d[col - 1])
-                   .thenComparing(Comparator.comparingInt((int[] d) -> d[0]).reversed()));
+        // Arrays.sort(data, Comparator.comparing((int[] d) -> d[col - 1])
+        //            .thenComparing(Comparator.comparingInt((int[] d) -> d[0]).reversed()));
+        Arrays.sort(data, (d1, d2) -> 
+                    d1[col - 1] == d2[col - 1] ? d2[0] - d1[0] : d1[col - 1] - d2[col - 1]
+                   );
                     
         
         int answer = calc(data[row_begin - 1], row_begin);

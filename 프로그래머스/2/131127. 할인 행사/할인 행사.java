86875b95a6bbdaac59ed4f9263555954413dc438
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-08-12 15:57:24 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,40 @@
+import java.util.*;
+/*
+처음부터 끝까지 가면서 
+*/
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        Map<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        Map<String, Integer> sliceMap = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            String str = discount[i];
+            sliceMap.put(str, sliceMap.getOrDefault(str, 0) + 1);
+        }
+        
+        for (int i = 9; i < discount.length; i++) {
+            String str = discount[i];
+            sliceMap.put(str, sliceMap.getOrDefault(str, 0) + 1);
+            if (i > 9) {
+                String preStr = discount[i - 10];
+                sliceMap.put(preStr, sliceMap.get(preStr) - 1);
+            }
+            boolean flag = false;
+            for (String key : wantMap.keySet()) {
+                if (wantMap.get(key) > sliceMap.getOrDefault(key, 0)) {
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

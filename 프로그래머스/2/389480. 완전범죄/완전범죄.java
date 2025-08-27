3bcf76600eb1901c5aa8c99c64442547aad25d62
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-08-27 18:43:17 +0900

    [level 2] Title: 완전범죄, Time: 0.04 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,29 +1,41 @@
+import java.util.*;
+/*
+[최초 풀이 전략]
+백트랙킹
+
+[풀이 전략]
+dp로 각 칸에 B도둑의 해당 누적 흔적일 때의 A도둑의 최소 누적 흔적값을 저장
+*/
 class Solution {
     
-    private int answer = -1;
-    
     public int solution(int[][] info, int n, int m) {
-        int[][][] visited = new int[info.length + 1][121][121];
-        backtrack(info, 0, n, m, 0, 0, visited);
-        return answer;
-    }
-    
-    private void backtrack(
-        int[][] info, int idx, int n, int m, int sumA, int sumB, int[][][] visited
-    ) {
-        if (sumA >= n || sumB >= m || visited[idx][sumA][sumB] == 1) {
-            return;
-        }
-        if (idx == info.length) {
-            if (answer == -1) {
-                answer = sumA;
-            } else {
-                answer = Math.min(answer, sumA);
+        int[] dp = new int[m];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for (int[] arr : info) {
+            int A = arr[0], B = arr[1];
+            int[] newDp = new int[m];
+            Arrays.fill(newDp, Integer.MAX_VALUE);
+            for (int b = 0; b < m; b++) {
+                if (dp[b] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                // A가 챙길 경우
+                if (dp[b] + A < n) {
+                    newDp[b] = Math.min(dp[b] + A, newDp[b]);
+                }
+                // B가 챙길 경우
+                if (b + B < m) {
+                    newDp[b + B] = Math.min(newDp[b + B], dp[b]);
+                }
             }
-            return;
+            dp = newDp;
         }
-        visited[idx][sumA][sumB] = 1;
-        backtrack(info, idx + 1, n, m, sumA + info[idx][0], sumB, visited);
-        backtrack(info, idx + 1, n, m, sumA, sumB + info[idx][1], visited);
+        int answer = Integer.MAX_VALUE;
+        for (int d : dp) {
+            answer = Math.min(answer, d);
+        }
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }

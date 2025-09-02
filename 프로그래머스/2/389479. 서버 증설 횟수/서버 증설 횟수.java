6f89832cd3a2892a450565d4a5c22105eae959b9
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-09-02 11:55:03 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,22 +1,26 @@
-import java.util.*;
+/*
+[풀이 전략]
+슬라이딩 윈도우 방식으로 스캔하면서 각 시간대에 활성화된 서버 수와 필요한 서버 수를 파악
+-> 몇 번 증설해야 하는 지 누적
 
+필요 추가 서버 수: 사람 수 / m
+*/
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        
-        int servers = 0;
+        int cur = 0;
         int[] record = new int[24];
         
-        for (int i = 0; i < players.length; i++) {
-            // 서버 비활성화
+        for (int i = 0; i < 24; i++) {
             if (i >= k) {
-                servers -= record[i - k];
+                cur -= record[i - k];
             }
-            // 0 ~ m-1 -> 0대 / m ~ 2m-1 -> 1대 / 2m ~ 3m-1 -> 2대
-            int addCnt = Math.max(players[i] / m - servers, 0);
-            answer += addCnt;
-            servers += addCnt;
-            record[i] = addCnt;
+            int need = players[i] / m;
+            if (need > cur) {
+                record[i] = need - cur;
+                cur += record[i];
+                answer += record[i];
+            }
         }
         
         return answer;

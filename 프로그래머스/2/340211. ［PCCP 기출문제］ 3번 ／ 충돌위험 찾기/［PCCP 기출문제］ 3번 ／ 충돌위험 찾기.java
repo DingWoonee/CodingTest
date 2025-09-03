b9dcdf11cf0908db0b6deaa930237dede111a219
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-09-03 11:24:46 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 91.94 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,72 +1,85 @@
+/*
+최단경로 이동, 행 이동 우선
+
+충돌 횟수를 구해야 함.
+
+[구현 전략]
+시뮬레이션
+*/
 class Solution {
-    // points 조회할 때는 -1하기
+    
+    int robots;
+    int deses;
+    
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
-        int robotNum = routes.length;
-        // 현재 위치
-        int[][] curPos = new int[routes.length][2];
-        // 현재 route 인덱스
-        int[] routeIdxs = new int[routes.length];
-        // 충돌 상태 체크를 위한 현위치 배열
+        robots = routes.length;
+        deses = routes[0].length;
         int[][] map = new int[101][101];
+        int[][] cur = new int[101][2];
+        int[] nextIdx = new int[101];
+        
+        // 최초 위치
+        for (int i = 0; i < robots; i++) {
+            int[] point = points[routes[i][0] - 1];
+            map[point[0]][point[1]]++;
+            cur[i][0] = point[0];
+            cur[i][1] = point[1];
+            nextIdx[i] = 1;
+        }
+        // 충돌 체크
+        answer += checkCollision(map);
         
         while (true) {
-            for (int i = 0; i < robotNum; i++) {
-                if (routeIdxs[i] >= routes[i].length) {
-                    continue;
-                }
-                // 최초
-                if (routeIdxs[i] == 0) {
-                    int[] point = points[routes[i][0] - 1];
-                    curPos[i][0] = point[0];
-                    curPos[i][1] = point[1];
-                    routeIdxs[i]++;
-                    map[curPos[i][0]][curPos[i][1]]++;
-                    continue;
-                }
-                // 목적지 도달
-                int[] desPos = points[routes[i][routeIdxs[i]] - 1];
-                if (curPos[i][0] == desPos[0] && curPos[i][1] == desPos[1]) {
-                    routeIdxs[i]++;
-                    if (routeIdxs[i] >= routes[i].length) {
-                        continue;
-                    }
-                    desPos = points[routes[i][routeIdxs[i]] - 1];
-                }
-                // 이동
-                if (desPos[0] > curPos[i][0]) {
-                    curPos[i][0]++;
-                } else if (curPos[i][0] > desPos[0]) {
-                    curPos[i][0]--;
-                } else if (desPos[1] > curPos[i][1]) {
-                    curPos[i][1]++;
-                } else {
-                    curPos[i][1]--;
-                }
-                map[curPos[i][0]][curPos[i][1]]++;
-            }
-            // 충돌 체크
-            int count = 0;
-            for (int i = 1; i < map.length; i++) {
-                for (int j = 1; j < map[0].length; j++) {
-                    if (map[i][j] > 1) {
-                        count++;
-                    }
-                    map[i][j] = 0;
-                }
-            }
-            answer += count;
-            // 완료 체크
-            int checkCount = 0;
-            for (int i = 0; i < robotNum; i++) {
-                if (routeIdxs[i] >= routes[i].length) {
-                    checkCount++;
-                }
-            }
-            if (checkCount == robotNum) {
+            // 이동
+            if (moveRobots(map, cur, nextIdx, points, routes) == robots) {
                 break;
             }
+            // 충돌 체크
+            answer += checkCollision(map);
         }
+        
         return answer;
+    }
+    
+    private int moveRobots(int[][] map, int[][] cur, int[] nextIdx, int[][] points, int[][] routes) {
+        int count = 0;
+        for (int i = 0; i < robots; i++) {
+            int[] pos = cur[i];
+            int desPoint = routes[i][nextIdx[i]];
+            int[] des = points[desPoint - 1];
+            if (pos[0] == des[0] && pos[1] == des[1]) { // 도착
+                if (nextIdx[i] == deses - 1) { // 완료
+                    count++;
+                    continue;
+                } else { // 다음 목적지
+                    des = points[routes[i][++nextIdx[i]] - 1];
+                }
+            }
+            if (pos[0] > des[0]) { // 행 우선 이동
+                pos[0]--;
+            } else if (des[0] > pos[0]) {
+                pos[0]++;
+            } else if (pos[1] > des[1]) {
+                pos[1]--;
+            } else {
+                pos[1]++;
+            }
+            map[pos[0]][pos[1]]++;
+        }
+        return count;
+    }
+    
+    private int checkCollision(int[][] map) {
+        int count = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 1) {
+                    count++;
+                }
+                map[i][j] = 0;
+            }
+        }
+        return count;
     }
 }

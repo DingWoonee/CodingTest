4b8c99f12995bad83a8701bfab0fe66ffab944f1
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-09-05 14:57:44 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 38.97 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,60 +1,57 @@
 import java.util.*;
 
 class Solution {
+    
+    int rows, cols;
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    
     public int solution(int[][] land) {
-        int rows = land.length, cols = land[0].length;
-        int[][] newLand = new int[rows][cols];
-        int[] a = {1, -1, 0, 0};
-        int[] b = {0, 0, 1, -1};
-        // 영역 구분을 위한 번호
-        int area = 1;
-        // 각 영역의 매장량
-        int[] map = new int[rows * cols + 1];
-        // 전처리
+        rows = land.length;
+        cols = land[0].length;
+        int mark = 1;
+        int[] counts = new int[250001];
+        int[][] marked = new int[rows][cols];
+        boolean[][] visited = new boolean[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (land[i][j] == 1) {
-                    Deque<int[]> stack = new ArrayDeque<>();
-                    stack.push(new int[]{i, j});
-                    land[i][j] = 0;
-                    int count = 0;
-                    while (!stack.isEmpty()) {
-                        int[] cord = stack.pop();
-                        int r = cord[0], c = cord[1];
-                        newLand[r][c] = area;
-                        count++;
-                        
-                        for (int k = 0; k < 4; k++) {
-                            int mr = r + a[k];
-                            int mc = c + b[k];
-                            if (mr < 0 || mr >= rows || mc < 0 || mc >= cols) {
-                                continue;
-                            }
-                            if (land[mr][mc] == 1) {
-                                stack.push(new int[]{mr, mc});
-                                land[mr][mc] = 0;
-                            }
+                if (visited[i][j] || land[i][j] == 0) {
+                    continue;
+                }
+                int count = 0;
+                
+                Deque<int[]> stack = new ArrayDeque<>();
+                stack.push(new int[]{i, j});
+                visited[i][j] = true;
+                while (!stack.isEmpty()) {
+                    int[] cur = stack.pop();
+                    marked[cur[0]][cur[1]] = mark;
+                    count++;
+                    for (int d = 0; d < 4; d++) {
+                        int x = cur[0] + dx[d], y = cur[1] + dy[d];
+                        if (x >= 0 && x < rows && y >= 0 && y < cols 
+                            && land[x][y] == 1 && !visited[x][y]) {
+                            stack.push(new int[]{x, y});
+                            visited[x][y] = true;
                         }
                     }
-                    map[area] = count;
-                    area++;
                 }
+                counts[mark] = count;
+                mark++;
             }
         }
-        // 탐색
+        
         int answer = 0;
-        for (int j = 0; j < cols; j++) {
-            int count = 0;
-            boolean[] visitedArea = new boolean[area + 1];
-            for (int i = 0; i < rows; i++) {
-                if (!visitedArea[newLand[i][j]]) {
-                    count += map[newLand[i][j]];
-                    visitedArea[newLand[i][j]] = true;
-                }
+        for (int i = 0; i < cols; i++) {
+            Set<Integer> set = new HashSet<>();
+            for (int j = 0; j < rows; j++) {
+                set.add(marked[j][i]);
             }
-            if (count > answer) {
-                answer = count;
+            int sum = 0;
+            for (int idx : set) {
+                sum += counts[idx];
             }
+            answer = Math.max(answer, sum);
         }
         return answer;
     }

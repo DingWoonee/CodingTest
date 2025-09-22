a8ff21e39690d49d5b58ee62e8723f52187ed829
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-09-22 14:45:14 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.59 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,72 +1,73 @@
+/*
+- 카드 숫자 1~n / 동전 coin개
+- 카드 n/3장을 뽑아 가짐.(n은 6의 배수)
+- 각 라운드 시작 시
+    - 카드를 두 장 뽑음
+    - 카드가 없다면 게임 종료
+    - 뽑은 카드를 가지려면 한 장에 동전 1개 내야됨
+    - 카드에 적힌 수의 합이 n+1이 되도록 카드 두 장을 내야 다음 라운드 진행 가능
+-------------------
+[풀이 전략]
+각 라운드에 카드를 살 지 버릴지 결정해야됨.
+최대 라운드는 333라운드 -> dfs/bfs 불가능.
+=> 각 라운드에 뽑은 카드로 따로 저장하고, 나중에 필요할 때 구매
+*/
 import java.util.*;
 
 class Solution {
-    Set<Integer> original, additional;
+    
     public int solution(int coin, int[] cards) {
-        int answer = 0;
-        int len = cards.length;
-        original = new HashSet();
-        additional = new HashSet();
-        
-        int idx = len / 3;
-        for(int i = 0 ; i < idx; ++i)
-            original.add(cards[i]);
-        
-        int target = len + 1;   
-        while(true){
-            answer++;
-            if(idx >= len){
-                break;
-            }
-            additional.add(cards[idx]);
-            additional.add(cards[idx+1]);
-            idx += 2;
+        Set<Integer> hand = new HashSet<>();
+        Set<Integer> added = new HashSet<>();
+        int sum = cards.length + 1;
+        // 초반 세팅
+        for (int i = 0; i < cards.length / 3; i++) {
+            hand.add(cards[i]);
+        }
+        // 라운드 진행
+        int round = 1;
+        for (int i = cards.length / 3; i < cards.length; i += 2) {
+            int card1 = cards[i];
+            int card2 = cards[i + 1];
+            added.add(card1);
+            added.add(card2);
+            // 검사
             boolean flag = false;
-            // Step1. 최초 카드에서 해결할 수 있는지 확인.
-            for(int i : original){
-                if(original.contains(target - i)){
-                    original.remove(i);
-                    original.remove(target - i);
+            for (int c : hand) {
+                if (hand.contains(sum - c)) {
+                    hand.remove(c);
+                    hand.remove(sum - c);
                     flag = true;
                     break;
                 }
             }
-            
-            // Step2. 최초 카드에서 해결이 안되었다면
-            if(!flag){
-                // 최초 카드와 라운드 추가 카드 1장을 이용해서 해결 할 수 있는지 확인.
-                if(coin > 0){ // 최소 1개 이상의 코인이 있어야 추가 카드를 받아서 사용할 수 있다.
-                    for(int i : original){
-                        if(additional.contains(target - i)){
-                            original.remove(i);
-                            additional.remove(target - i);
-                            --coin;
-                            flag = true;
-                            break;
-                        }
+            if (!flag && coin > 0) {
+                for (int c : hand) {
+                    if (added.contains(sum - c)) {
+                        coin--;
+                        hand.remove(c);
+                        added.remove(sum - c);
+                        flag = true;
+                        break;
                     }
                 }
             }
-            
-            // Step3. 그래도 해결이 안되었다면, 추가 카드들 간에 해결이 가능한 지 확인.
-            if(!flag){
-                if(coin > 1){ // 최소 2개 이상의 코인이 있어야 추가 카드를 중에서 해결이 가능하다.
-                    for(int i : additional){
-                        if(additional.contains(target - i)){
-                            additional.remove(i);
-                            additional.remove(target - i);
-                            coin -= 2;
-                            flag = true;
-                            break;
-                        }
+            if (!flag && coin > 1) {
+                for (int c : added) {
+                    if (added.contains(sum - c)) {
+                        coin -= 2;
+                        added.remove(c);
+                        added.remove(sum - c);
+                        flag = true;
+                        break;
                     }
                 }
             }
-            
-            // 완성되지 않았다.
-            if(!flag)
+            if (!flag) {
                 break;
+            }
+            round++;
         }
-        return answer;
+        return round;
     }
 }

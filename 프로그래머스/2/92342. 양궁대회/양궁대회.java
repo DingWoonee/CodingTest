18980e43417651c69754a7de22893daf9d6e3a3b
김성운 <136048882+DingWoonee@users.noreply.github.com>
2026-02-18 15:01:21 +0900

    [level 2] Title: 양궁대회, Time: 9.04 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,69 +1,55 @@
-import java.util.*;
-/*
-최대 11_C_5 = 462
-*/
 class Solution {
     
-    int[] answer;
-    int maxDiff = 0;
-    
-    int arrowN;
-    int[] pInfo;
+    private int INF = Integer.MIN_VALUE;
+    private int maxDiff = INF;
+    private int[] ans = new int[11];
+    private int[] info;
     
     public int[] solution(int n, int[] info) {
-        answer = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, n};
-        arrowN = n;
-        pInfo = info;
+        this.info = info;
         
-        dfs(0, new int[11], 0);
+        comb(n, n, 10, new int[11]);
         
-        if (maxDiff == 0) {
-            return new int[]{-1};
-        }
-        return answer;
+        return maxDiff == INF ? new int[]{-1} : ans;
     }
     
-    private void dfs(int count, int[] scores, int step) {
-        if (count == arrowN) {
-            int diff = calculate(scores);
-            if (diff == maxDiff) {
-                for (int i = 10; i >= 0; i--) {
-                    if (answer[i] > scores[i]) {
-                        break;
-                    }
-                    if (scores[i] > answer[i]) {
-                        answer = scores.clone();
-                        break;
-                    }
-                }
-            } else if (diff > maxDiff) {
-                maxDiff = diff;
-                answer = scores.clone();
+    private void comb(int n, int rest, int cur, int[] lion) {
+        if (rest == 0) {
+            int diff = calcDiff(lion);
+            if (diff <= 0 || maxDiff > diff) return;
+            if (maxDiff == diff && !check(lion)) {
+                return;
             }
+            maxDiff = diff;
+            for (int i = 0; i < lion.length; i++) ans[i] = lion[i];
             return;
         }
         
-        if (step == 11) {
-            return;
+        for (int i = cur; i >= 0; i--) {
+            lion[10 - i]++;
+            comb(n, rest - 1, i, lion);
+            lion[10 - i]--;
         }
-        
-        for (int i = 0; i <= arrowN - count; i++) {
-            scores[step] = i;
-            dfs(count + i, scores, step + 1);
-        }
-        scores[step] = 0;
     }
     
-    private int calculate(int[] lion) {
-        int pSum = 0, lSum = 0;
+    private boolean check(int[] lion) {
+        for (int i = 10; i >= 0; i--) {
+            if (ans[i] > lion[i]) return false;
+            if (lion[i] > ans[i]) return true;
+        }
+        return false;
+    }
+    
+    private int calcDiff(int[] lion) {
+        int apeachS = 0;
+        int lionS = 0;
+        
         for (int i = 0; i < 11; i++) {
-            if (lion[10 - i] > pInfo[10 - i]) {
-                lSum += i;
-            } else if (pInfo[10 - i] > 0) {
-                pSum += i;
-            }
+            int score = 10 - i;
+            if (lion[i] > info[i]) lionS += score;
+            else if (info[i] > 0) apeachS += score;
         }
         
-        return Math.max(0, lSum - pSum);
+        return lionS - apeachS;
     }
 }

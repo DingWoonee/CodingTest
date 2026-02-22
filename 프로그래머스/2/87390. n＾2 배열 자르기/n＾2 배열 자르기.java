e928f8e55301aa7cccf72f5bc5dff649a3243fd5
김성운 <136048882+DingWoonee@users.noreply.github.com>
2026-02-22 23:11:16 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.73 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,28 +1,17 @@
-/*
-시작행 - left / n
-끝행 - right / n
-k행의 요소 - k가 k개, k + 1, k + 2, ..., n
+/**
+* i 행 = i가 i개, i + 1, i + 2, ...
 */
 class Solution {
     public int[] solution(int n, long left, long right) {
-        int len = (int) (right - left) + 1;
-        int start = (int) (left / n) + 1;
-        int end = (int) (right / n) + 1;
-        int[] answer = new int[len];
-        int idx = 0;
-        for (int i = start; i <= end; i++) {
-            for (long j = Math.max(left, (long) (i - 1) * n); 
-                 j <= Math.min(right, (long) i * n - 1); 
-                 j++
-            ) {
-                int num = (int) ((j + 1 - (long) (i - 1) * (long) n));
-                if (num > i) {
-                    answer[idx++] = num;
-                } else {
-                    answer[idx++] = i;
-                }
-            }
+        int[] ans = new int[(int) (right - left) + 1];
+        
+        int p = 0;
+        for (long i = left; i <= right; i++) {
+            int row = (int) (i / n + 1);
+            int col = (int) (i % n + 1);
+            ans[p++] = col <= row ? row : row + (col - row);
         }
-        return answer;
+        
+        return ans;
     }
 }

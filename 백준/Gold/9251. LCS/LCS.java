3ff1dc7cc4dacc5289df730262c8d2d5d08eb137
김성운 <136048882+DingWoonee@users.noreply.github.com>
2026-03-05 16:35:57 +0900

    [Gold IV] Title: LCS, Time: 148 ms, Memory: 19148 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+/**
+ * CAPCAK
+ * ACAYKP
+ * dp[i][j] = 첫 문자열의 i번째까지 보고, 두 번째 문자열의 j번째까지 봤을 때의 최대 길이
+ */
+public class Main {
+
+    private static char[] ch1;
+    private static char[] ch2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ch1 = br.readLine().toCharArray();
+        ch2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[ch1.length + 1][ch2.length + 1];
+        for (int i = 0; i < ch1.length; i++) {
+            for (int j = 0; j < ch2.length; j++) {
+                dp[i + 1][j + 1] = dp[i][j + 1];
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i + 1][j]);
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + (ch1[i] == ch2[j] ? 1 : 0));
+            }
+        }
+
+        System.out.println(dp[ch1.length][ch2.length]);
+    }
+}
